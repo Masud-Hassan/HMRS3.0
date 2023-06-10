2eac830437b037f72c0bec9e4c41d948d27e1201
Masud-Hassan <masudhassaniatb64@hotmail.com>
2023-06-10 21:35:35 +0600

    first entry for the third time

---
+++ resources/views/login.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="css/login.css" />
+  </head>
+  <body>
+    <div style="position:absolute; top: 3%; right: 3%">
+      <button class="apply">Apply For Job</button>
+    </div>
+    <div>
+      <div style="display:flex; align-items:center; justify-content:center">
+          <img src="images/logo.png" alt="hrms" style="width: 200px; height:auto">
+      </div>
+      
+      <div>
+        <br>
+      </div>
+      <div class="container">
+          <header style="color: #826AFB; font-size: 300%">LOGIN</header>
+          <form action="#" class="form">
+            <div class="input-box">
+              <label>Email</label>
+              <input type="text" placeholder="Enter email address" required />
+            </div>
+
+            <div class="input-box">
+              <label>Password</label>
+              <input type="text" placeholder="Enter Password" required />
+            </div>
+
+            <div class="gender-box">
+              <div class="gender-option">
+                <div class="gender">
+                  <input type="checkbox" id="check-male" name="remember" checked />
+                  <label for="check-male">Remember me?</label>
+                </div>
+                <div class="gender">
+                    <p>Forgot Pass?</p><a href="#" id="check-other" style="float: right">Contact Admin.</a>
+                </div>
+              </div>
+            </div>
+            
+            <button>Submit</button>
+          </form>
+      </div>   
+    </div>
+  </body>
+</html>
